e52e5a4c4718bb4033ab1d9f90a94b5889482d3b
Levijs <levijskal@icloud.com>
2020-01-28 14:17:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.992c589d51ef198bed6d17bb8f74b600.js"
+  "/precache-manifest.8e522f32f4d8d1c7f40bfd362c12bfac.js"
 );
 
 self.addEventListener('message', (event) => {
